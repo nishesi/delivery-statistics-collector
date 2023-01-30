3c61ca52c1124c4cd9d643ca41cfc4ff88b0fe68
Nurislam Zaripov <znurislama56@gmail.com>
2023-01-30 16:20:08 +0300

    create new work information tables

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,9 +1,12 @@
 drop table if exists authorities;
 drop table if exists users;
 
+-- Common tables
+
 create table users
 (
-    email    varchar(50) primary key,
+    id       BIGSERIAL PRIMARY KEY,
+    email    varchar(50) UNIQUE,
     username varchar(50)  not null,
     password varchar(500) not null
 );
@@ -15,3 +18,35 @@ create table authorities
     constraint fk_authorities_users foreign key (email) references users (email) on delete cascade
 );
 create unique index ix_auth_username on authorities (email, authority);
+
+
+
+-- Work tables
+
+CREATE TABLE work_days
+(
+    id            BIGSERIAL PRIMARY KEY,
+    user_id       BIGINT REFERENCES users (id),
+    day           DATE NOT NULL,
+    delivery_gain INT  NOT NULL CHECK ( delivery_gain >= 0 ),
+    comment       VARCHAR(255)
+);
+
+CREATE TABLE shifts
+(
+    id            BIGSERIAL PRIMARY KEY,
+    day_id        BIGINT REFERENCES work_days (id),
+    interval      INTERVAL      NOT NULL,
+    average_ratio NUMERIC(4, 2) NOT NULL CHECK ( average_ratio >= 0 ),
+    orders_count  SMALLINT CHECK (orders_count >= 0),
+    gain          INT CHECK (gain >= 0),
+    distance      NUMERIC(4, 2) NOT NULL CHECK (distance >= 0)
+);
+
+CREATE TABLE expenses
+(
+    id     BIGSERIAL PRIMARY KEY,
+    day_id BIGINT REFERENCES work_days (id),
+    name   VARCHAR(50) NOT NULL,
+    value  SMALLINT    NOT NULL CHECK ( value > 0 )
+);
