ea8bf942abd42a911794577f517ddba7e8a078a1
Nurislam Zaripov <znurislama56@gmail.com>
2023-01-30 12:27:39 +0300

    update tables

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -3,15 +3,15 @@ drop table if exists users;
 
 create table users
 (
-    email varchar(50)  not null primary key,
-    username varchar(50) not null,
+    email    varchar(50) primary key,
+    username varchar(50)  not null,
     password varchar(500) not null
 );
 
 create table authorities
 (
-    email  varchar(50) not null,
+    email     varchar(50) not null,
     authority varchar(50) not null,
-    constraint fk_authorities_users foreign key (email) references users (email)
+    constraint fk_authorities_users foreign key (email) references users (email) on delete cascade
 );
 create unique index ix_auth_username on authorities (email, authority);
